867197f9f8d8a0d5040210ad34d1528c8e74ed80
S00190873 <S00190873@mail.itsligo.ie>
2021-01-22 22:44:54 +0000

    prevention of duplicate usernames and emails complete

--- expressapp/routes/users.js
+++ expressapp/routes/users.js
@@ -9,13 +9,9 @@ var passport = require('passport');
 //   res.send('respond with a resource');
 // });
 
-router.post('/register', function (req, res, next) {
-  addToDB(req, res);
-});
-
-
-async function addToDB(req, res) {
-
+// Register
+router.post("/register", async (req, res, next) => {
+  //store the users as variables for easier use from the JSON request
   var user = new User({
     email: req.body.email,
     username: req.body.username,
@@ -23,6 +19,51 @@ async function addToDB(req, res) {
     creation_dt: Date.now()
   });
 
+  console.dir(user);
+  console.log("START OF METHOD");
+
+
+   //Validate userName
+   const validateUsername = async (username) => {
+    let user = await User.findOne({ username });
+    return user ? false : true;
+  };
+
+  //#region validateEmail
+  const validateEmail = async (email) => {
+    let user = await User.findOne({ email });
+    return user ? false : true;
+  };
+  //#endregion validateEmail
+
+  // Validate the username
+  let usernameNotTaken = await validateUsername(user.username);
+  console.log("USERNAME AVAILABLE: " + usernameNotTaken);
+  if (!usernameNotTaken) {
+    // not-not-taken means it is taken because it is a double negative
+    return res.json({
+      //bad request
+      msg: `Username unavailable.`,
+      success: false,
+    });
+  }
+
+  // Validate the email
+  let emailNotRegistered = await validateEmail(user.email);
+  console.log("EMAIL AVAILABLE: " + emailNotRegistered);
+  if (!emailNotRegistered) {
+    return res.json({
+      msg: `This email is already associated with a registered account.`,
+      success: false,
+    });
+  }
+  addToDB(user, res); 
+  }
+);
+
+
+
+async function addToDB(user, res) {
   try {
     doc = await user.save();
     return res.status(201).json(doc);
