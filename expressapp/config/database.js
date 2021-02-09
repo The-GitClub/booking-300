fadd27f4b867f2239c8917448e12819cc530302e
Aaron Dollard <daronaollard1995@gmail.com>
2021-02-09 22:30:00 +0000

    changed db name

--- expressapp/config/database.js
+++ expressapp/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/testenv',
+    database: 'mongodb://localhost:27017/booking300development',
     SECRET: 'booking300',
     dbCollection: 'bookings'
 }
